f1163c7a8ebbd8865752607d8893fdcdbd963198
Guillaume Ader <guillaume@Guillaumes-MacBook-Pro-2.local>
2019-06-29 12:30:28 +0100

    deinfe requirements

--- index.js
+++ index.js
@@ -28,13 +28,33 @@
  *    - list of moves, (m or r)
  *    e.g. [Array] = [ 'r', 'm', 'm', 'r']... 
  * 
- * If the position brings out of the map... I will return false anyway Out of bound
+ * I will assume the following rules:
+ *  - If the position brings out of the map, I will stop the game and return out of bound message, even if there is some moves left
+ *  - if the tutle touches a mine, I will stop the game and return mine hit message
+ *  - if the tutle passes on the exit point, I will stop the game even if there is still some moves left 
  * 
+ * Input handler
+ *  - for dev and testabilty purpose, I will store moves/settings as an Array of moves/settings - so I can run multiples scenerio at once
+ *  - I will assume the inputs are valid format:
+ *      - moves is array 0 < moves.length < 1000
+ *      - moves[i] will be either 'r' or 'm' in lowercase
+ *  - in Settings
+ *      - Grid { height, width } = positive integer, 1 < height, width < 1000
+ *      - start position define as {Coordinates}
+ *      - start postition will always be valide coordinates (within the grid dimenssion)
+ *      - exit position define as {Coordinates}
+ *      - exit postition will always be valide coordinates (within the grid dimenssion)
+ *      - Coordinates defines as an object with x and y properties
+ *      - x and y properties will always be positive integer and0 < x,y < 1000
+ *      - mines will be a list of {coordinates} and all within the grid dimenssion
  */
 
 const settings = require('./config/settings.json')
 const moves = require('./config/moves.json')
 const Manager = require('./src/Manager')
 
-const start = new Manager(settings, moves)
+const runGames = new Manager(settings, moves)
+runGames.forEach((result, index) => {
+  console.log(`result round ${index}: ${result}`)
+});
 
