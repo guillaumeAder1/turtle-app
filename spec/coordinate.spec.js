6b5f89096e5e9a89a5724b6214a1ac6de738c740
Guillaume Ader <guillaume@Guillaumes-MacBook-Pro-2.local>
2019-06-30 17:08:41 +0100

    use to equal

--- spec/coordinate.spec.js
+++ spec/coordinate.spec.js
@@ -8,14 +8,14 @@ describe('Coordinate module', () => {
   })
   it('should have correct inputs & properties', () => {
     const coord = new Coordinate(3, 2)
-    expect(coord.x).toBe(3)
-    expect(coord.y).toBe(2)
+    expect(coord.x).toEqual(3)
+    expect(coord.y).toEqual(2)
   })
   it('should return valid coordinates as a string', () => {
     const coord = Coordinate.coordToString({ x: 4, y: 5 })
-    expect(coord).toBe('4_5')
+    expect(coord).toEqual('4_5')
     const coord2 = Coordinate.coordToString({ x: '4', y: '5' })
-    expect(coord2).toBe('4_5')
+    expect(coord2).toEqual('4_5')
   })
   it('should return false if coordinate are not valid', () => {
     let coord = Coordinate.coordToString({ x: 4 })
